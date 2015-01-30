d524caf9f9279afaaddf2b4a4b29fdce4e228bd8
Piotr Dyba <piotr@dyba.com.pl>
2015-01-30 07:14:03 +0100

    fixed dates

--- src/lunch_app/forms.py
+++ src/lunch_app/forms.py
@@ -54,8 +54,6 @@ class OrderForm(Form):
         ]
     )
     send_me_a_copy = BooleanField('send_me_a_copy', default=False)
-    today_date = datetime.now().replace(microsecond=0)
-    date = DateField(default=today_date, format='%Y-%m-%d')
 
 
 class OrderEditForm(OrderForm):
@@ -118,7 +116,6 @@ class AddFood(Form):
                 ),
         ]
     )
-    today_date = date.today()
     date_available_from = DateField(
         label='date_available',
         default=datetime(2015, 1, 1, 0, 0, 0),
